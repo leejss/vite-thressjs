4fb20d71b26d2cf91b2c0689b8fea22af2555295
leejss <goldemshine@gmail.com>
2022-12-11 23:05:17 +0900

    update boilerplate

--- main.js
+++ main.js
@@ -1,4 +1,54 @@
 import * as THREE from "three";
+import { OrbitControls } from "three/examples/jsm/controls/OrbitControls";
+import "./style.css";
+
+const sizes = {
+  width: innerWidth,
+  height: innerHeight,
+};
+
+// Cursor
+const cursor = {
+  x: 0,
+  y: 0,
+};
+const canvas = document.querySelector(".webgl");
+
+addEventListener("mousemove", (e) => {
+  cursor.x = e.clientX / sizes.width - 0.5;
+  cursor.y = -(e.clientY / sizes.height - 0.5);
+});
+
+// Handle resize
+addEventListener("resize", (e) => {
+  sizes.width = innerWidth;
+  sizes.height = innerHeight;
+
+  // update camera
+  camera.aspect = sizes.width / sizes.height;
+  camera.updateProjectionMatrix();
+  // Update renderer
+  renderer.setSize(sizes.width, sizes.height);
+  renderer.setPixelRatio(Math.min(devicePixelRatio, 2));
+});
+
+addEventListener("dblclick", () => {
+  const fullscreenElement = document.fullscreenElement || document.webkitFullscreenElement;
+
+  if (!fullscreenElement) {
+    if (canvas.requestFullscreen) {
+      canvas.requestFullscreen();
+    } else if (canvas.webkitRequestFullscreen) {
+      canvas.webkitRequestFullscreen();
+    }
+  } else {
+    if (document.exitFullscreen) {
+      document.exitFullscreen();
+    } else if (document.webkitExitFullscreen) {
+      document.webkitExitFullscreen();
+    }
+  }
+});
 
 // Scene
 const scene = new THREE.Scene();
@@ -12,21 +62,35 @@ const mat = new THREE.MeshBasicMaterial({
 const mesh = new THREE.Mesh(geo, mat);
 scene.add(mesh);
 
-const sizes = {
-  width: 800,
-  height: 600,
-};
-
 // Camera
 const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height);
 camera.position.z = 3;
 scene.add(camera);
 
+// Conrols
+const controls = new OrbitControls(camera, canvas);
+controls.enableDamping = true;
+
 // Renderer
 
-const canvas = document.querySelector(".webgl");
 const renderer = new THREE.WebGLRenderer({
   canvas,
 });
 renderer.setSize(sizes.width, sizes.height);
+
 renderer.render(scene, camera);
+
+const clock = new THREE.Clock();
+
+const tick = () => {
+  const elapsedTime = clock.getElapsedTime();
+  // Update cameara
+
+  // Update controls
+  controls.update();
+
+  renderer.render(scene, camera);
+  requestAnimationFrame(tick);
+};
+
+tick();
